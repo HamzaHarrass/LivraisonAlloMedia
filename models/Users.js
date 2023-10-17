0112730859aa89d8004c383fb2f14b23b69d5e98
HamzaHarrass <hamzaharrass05@gmail.com>
2023-10-17 12:00:16 +0100

    ajouter model avec schema de user et role

---
+++ models/Users.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username : {
+        type : String,
+        require : true
+    },
+    email : {
+        type : String,
+        require : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        require : true
+    },
+    role : {
+        type : Schema.Types.ObjectId,
+        ref : 'Role'
+    },
+})
+
+const User = mongoose.model('User',userSchema);
+module.exports = {User};
