f663352e0241dbe06492e2072a9ad7ff7fe0e3f4
HamzaHarrass <hamzaharrass05@gmail.com>
2023-10-26 11:30:58 +0100

    ajouter packager cors qui permer de donner l'autorisation d'acces de consomer api

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require("express");
 require("./config/DBConnection")()
 const app = express();
 const authRouter = require("./routes/authRouter");
+const cors = require('cors');
+
 
 app.use(express.json());
 require('dotenv').config();
 
-
+app.use(cors({
+    origin : "http://localhost:5173"
+}))
 app.get('/',(req,res)=>{
     res.status(200).json({message:"ALL IS GOOD"})
 })
